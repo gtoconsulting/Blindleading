29a216677c35690a080af5a0c3629f62ffb996a5
Glenno <gtoconsulting@ntlworld.com>
2013-10-04 13:27:24 +0100

    root handler for homepage

--- app.js
+++ app.js
@@ -2,8 +2,16 @@ var express = require("express");
 var app = express();
 app.use(express.logger());
 
+app.get('/', function(request, response) {
+  response.send('Homepage');
+});
+
 app.get('/myfirst', function(request, response) {
-  response.send('Carlos=Master!');
+  response.send('Glenn=Master!');
+});
+
+app.get('/mysecond', function(request, response) {
+  response.send('Carlos=HeadstandMaster!');
 });
 
 var port = process.env.PORT || 5000;
